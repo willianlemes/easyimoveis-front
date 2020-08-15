c350110610f3f5ccad6fc52a2efb4d3e670cb4bc
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-08-15 19:44:35 -0300

    Adiciona estilos globais

---
+++ src/styles/index.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
