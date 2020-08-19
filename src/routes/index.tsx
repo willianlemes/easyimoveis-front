59edbcab5965389ff684dd5781cf5b09e2a1daef
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-08-18 23:29:55 -0300

    Adicionando novos containers em src/pages/Immobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Immobile from '../pages/Immobile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/immobile" component={Immobile} />
   </Switch>
 );
 
