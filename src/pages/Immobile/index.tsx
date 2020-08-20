60065857e63db7c1265117ee99cde9f99295f4cb
Vinicius Passoni <viniciuspassoni97@gmail.com>
2020-08-19 23:48:53 -0300

    Iniciando formulario de cadastro com styled-components

--- src/pages/Immobile/index.tsx
+++ src/pages/Immobile/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
-
+import { Divider } from '@material-ui/core';
 import FormContainer from '../../containers/FormContainer';
+import { ContainerTab, ContainerForm } from './Style';
 
 const Immobile: React.FC = () => (
   <FormContainer title="Cadastro de Imoveis" icon={<HomeWorkIcon />}>
-    <h1>Formulários aqui</h1>
+    <Divider />
+    <ContainerTab>
+      <h1>TAB1</h1>
+      <h1>TAB2</h1>
+    </ContainerTab>
+    <ContainerForm>
+      <h2>Sobre o Imóvel</h2>
+      <p>Proprietário</p>
+    </ContainerForm>
   </FormContainer>
 );
 
