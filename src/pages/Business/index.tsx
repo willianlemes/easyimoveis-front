b011eae1d14ad1f9f0e98187e208643362519b73
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-08-22 00:56:56 -0300

    Ajusta ícone para MenuItem Negócios

--- src/pages/Business/index.tsx
+++ src/pages/Business/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import WorkOutlinedIcon from '@material-ui/icons/WorkOutlined';
 
 import FormContainer from '../../containers/FormContainer';
 
 import Fieldset from '../../components/Fieldset';
 
 const Business: React.FC = () => (
-  <FormContainer title="Cadastro de Negócio" icon={<PersonAddIcon />}>
+  <FormContainer title="Cadastro de Negócio" icon={<WorkOutlinedIcon />}>
     <Fieldset legend="Cadastro">
       <h1>Fields goes here</h1>
     </Fieldset>
