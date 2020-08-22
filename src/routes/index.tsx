81cd20c0093056cc059d717b929e8ed003edfdd1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-08-22 00:54:17 -0300

    Ajusta rotas para Negocios, Pessoas e Imóveis

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Immobile from '../pages/Immobile';
+import Business from '../pages/Business';
+import People from '../pages/People';
+import Properties from '../pages/Properties';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/immobile" component={Immobile} />
+    <Route path="/negocios" exact component={Business} />
+    <Route path="/pessoas" exact component={People} />
+    <Route path="/propriedades" exact component={Properties} />
   </Switch>
 );
 
