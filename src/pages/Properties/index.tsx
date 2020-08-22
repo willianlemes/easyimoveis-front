ea8ea5e52c18bfb89014016ef51bbe5702f06f92
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-08-22 00:58:07 -0300

    Ajusta ícone para Properties

--- src/pages/Properties/index.tsx
+++ src/pages/Properties/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import HomeIcon from '@material-ui/icons/Home';
 
 import FormContainer from '../../containers/FormContainer';
 
 import Fieldset from '../../components/Fieldset';
 
 const Properties: React.FC = () => (
-  <FormContainer title="Cadastro de Imóveis" icon={<HomeWorkIcon />}>
+  <FormContainer title="Cadastro de Imóveis" icon={<HomeIcon />}>
     <Fieldset legend="Cadastro">
       <h1>Fields goes here</h1>
     </Fieldset>
