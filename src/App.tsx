031bceefb2da18ea7beef16c5af4701bb3a3050f
willianlemes <willianlemes94@gmail.com>
2020-10-30 11:48:58 -0300

    Limpeza de código no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Grid, Container, makeStyles } from '@material-ui/core';
-
-import Menu from './containers/Menu';
-import Routes from './routes';
-
-import GlobalStyle from './styles';
-
-const useStyles = makeStyles({
-  root: {
-    height: '80vh',
-    display: 'flex',
-    alignItems: 'center'
-  }
-});
 
 const App: React.FC = () => {
-  const classes = useStyles();
-
-  return (
-    <>
-      <GlobalStyle />
-
-      <BrowserRouter>
-        <Container className={classes.root}>
-          <Grid container spacing={4}>
-            <Grid item lg={3}>
-              <Menu />
-            </Grid>
-            <Grid item lg={9}>
-              <Routes />
-            </Grid>
-          </Grid>
-        </Container>
-      </BrowserRouter>
-    </>
-  );
+  return <div />;
 };
 
 export default App;
